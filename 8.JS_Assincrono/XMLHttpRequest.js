f35b1f356b21fdd8eaf640762ed35f97cc31b2ab
DreyIMM <andreydario26@gmail.com>
2022-09-04 16:03:37 -0300

    criando req com Fetch sem async

--- 8.JS_Assincrono/XMLHttpRequest.js
+++ 8.JS_Assincrono/XMLHttpRequest.js
@@ -1,25 +1,24 @@
-const request = obj =>{
-    return new Promise((resolve, reject)=>{
+// const request = obj =>{
+//     return new Promise((resolve, reject)=>{
 
-    const xhr = new XMLHttpRequest();
-    xhr.open(obj.method, obj.url, true);
-    xhr.send();
-    xhr.addEventListener('load', ()=>{
-        if(xhr.status >= 200 && xhr.status <300){
-            //funçaõ de callBac de sucesso
-            resolve(xhr.responseText);
-        }else{
-            //função de callBac de erro
-            reject(xhr.statusText);
-        }
-    })
+//     const xhr = new XMLHttpRequest();
+//     xhr.open(obj.method, obj.url, true);
+//     xhr.send();
+//     xhr.addEventListener('load', ()=>{
+//         if(xhr.status >= 200 && xhr.status <300){
+//             //funçaõ de callBac de sucesso
+//             resolve(xhr.responseText);
+//         }else{
+//             //função de callBac de erro
+//             reject(xhr.statusText);
+//         }
+//     })
         
-    })
+//     })
     
-}
+// }
 
 document.addEventListener('click', e=>{
-    debugger
     const el = e.target;
     const tag = el.tagName.toLowerCase();
     if(tag === 'a'){
@@ -31,25 +30,11 @@ document.addEventListener('click', e=>{
 
 async function carregaPagina(el){
     const href = el.getAttribute('href');
-    
-    const ObjConfig ={
-        method: 'GET',
-        url: href,
-    }
-    try {
-        const response = await request(ObjConfig);
-        carregaResultado(response);
-    } catch (error) {
-        console.log(error)
-    }
-   
 
-    /* request(ObjConfig).then(response =>{
-        carregaResultado(response);
-    }).catch(error =>{
-        console.log(error);
-    })
-    */
+    fetch(href)
+    .then(response  => response.text())
+    .then(html =>     carregaResultado(html))
+    .catch(e => console.error(e))
     
 }
 
